9fd674fdf960acb832dc091b694e85bd44a8c8b5
Kenneth <lordlqft0@gmail.com>
2026-05-12 16:55:12 -0300

    Refactor: Reformulação dos métodos e uso de boa prática
    Método de calcular descontos e ler valor da compra

---
+++ Atividade4.cs
@@ -0,0 +1,41 @@
+/*
+static string LerCliente()
+{
+    Console.Write("Digite o nome do cliente: ");
+    return Console.ReadLine()!;
+}
+
+static double LerValorCompra()
+{
+    Console.Write("Digite o valor da compra: ");
+    return double.Parse(Console.ReadLine()!);
+}
+
+static double CalcularDesconto(double valorCompra)
+{
+    double desconto = 0;
+
+    if (valorCompra > 100)
+    {
+        desconto = valorCompra * 0.1;
+    }
+    else
+    {
+        desconto = valorCompra * 0.05;
+    }
+    double valorFinal = valorCompra - desconto;
+    return valorFinal;
+}
+
+static void ExibirResultado()
+{
+    string cliente = LerCliente();
+    double valorCompra = LerValorCompra();
+    double valorFinal = CalcularDesconto(valorCompra);
+
+    Console.WriteLine($"Cliente: {cliente}");
+    Console.WriteLine($"Valor da compra: {valorCompra:F2}");
+    Console.WriteLine($"Desconto aplicado: {valorCompra - valorFinal:F2}");
+    Console.WriteLine($"Valor final da compra: {valorFinal:F2}");
+}
+*/
