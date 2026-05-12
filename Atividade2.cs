24aa860477e68e7104b6fb2b1b6a02109b7ec52a
Kenneth <lordlqft0@gmail.com>
2026-05-12 16:53:08 -0300

    Feat: Atividade 2 completa

---
+++ Atividade2.cs
@@ -0,0 +1,45 @@
+
+/*
+Um professor deseja criar um programa para ajudar alunos do ensino fundamental a 
+estudar tabuada. O sistema deve permitir que o usuário escolha um número e exibir 
+a tabuada de 1 até 10.
+*/
+
+/*
+static int LerNumero(string mensagem)
+{
+    Console.Write(mensagem);
+    int numero = int.Parse(Console.ReadLine()!);
+    return numero;
+}
+
+static void ExibirTabuada(int numero)
+{
+    Console.WriteLine($"Tabuada do {numero}:");
+    int tabuada = 0;
+    while (tabuada <= 10)
+    {
+        Console.WriteLine($"{numero} x {tabuada} = {numero * tabuada}");
+        tabuada++;
+    }
+    Console.WriteLine("Espere: 3");
+    Task.Delay(1000).Wait();
+    Console.WriteLine("Espere: 2");
+    Task.Delay(1000).Wait();
+    Console.WriteLine("Espere: 1");
+    Task.Delay(1000).Wait();
+    Console.Clear();
+}
+
+static void ExecutarSistema()
+{
+    while (true)
+    {
+        int numero = LerNumero("Digite um número para ver a tabuada: ");
+        ExibirTabuada(numero);
+    }
+
+}
+
+ExecutarSistema();
+*/
